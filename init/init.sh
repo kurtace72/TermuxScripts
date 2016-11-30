df40b9492e4271a0a4ef5fe4794ba66f55530475
Rodrigo Rosmaninho <quico.rosmaninho@gmail.com>
2016-11-30 17:54:31 +0000

    changed ascii art

--- init/init.sh
+++ init/init.sh
@@ -19,20 +19,12 @@ while true; do
     esac
 done
 
-echo "   SSSSSSSSSSSSSSSKKKKKKKKK    KKKKKKYYYYYYY       YYYYYYY      GGGGGGGGGGGGUUUUUUUU     UUUUUUUYYYYYYY       YYYYYYY";
-echo " SS:::::::::::::::K:::::::K    K:::::Y:::::Y       Y:::::Y   GGG::::::::::::U::::::U     U::::::Y:::::Y       Y:::::Y";
-echo "S:::::SSSSSS::::::K:::::::K    K:::::Y:::::Y       Y:::::Y GG:::::::::::::::U::::::U     U::::::Y:::::Y       Y:::::Y";
-echo "S:::::S     SSSSSSK:::::::K   K::::::Y::::::Y     Y::::::YG:::::GGGGGGGG::::UU:::::U     U:::::UY::::::Y     Y::::::Y";
-echo "S:::::S           KK::::::K  K:::::KKYYY:::::Y   Y:::::YYG:::::G       GGGGGGU:::::U     U:::::UYYY:::::Y   Y:::::YYY";
-echo "S:::::S             K:::::K K:::::K     Y:::::Y Y:::::Y G:::::G              U:::::D     D:::::U   Y:::::Y Y:::::Y   ";
-echo " S::::SSSS          K::::::K:::::K       Y:::::Y:::::Y  G:::::G              U:::::D     D:::::U    Y:::::Y:::::Y    ";
-echo "  SS::::::SSSSS     K:::::::::::K         Y:::::::::Y   G:::::G    GGGGGGGGGGU:::::D     D:::::U     Y:::::::::Y     ";
-echo "    SSS::::::::SS   K:::::::::::K          Y:::::::Y    G:::::G    G::::::::GU:::::D     D:::::U      Y:::::::Y      ";
-echo "       SSSSSS::::S  K::::::K:::::K          Y:::::Y     G:::::G    GGGGG::::GU:::::D     D:::::U       Y:::::Y       ";
-echo "            S:::::S K:::::K K:::::K         Y:::::Y     G:::::G        G::::GU:::::D     D:::::U       Y:::::Y       ";
-echo "            S:::::KK::::::K  K:::::KKK      Y:::::Y      G:::::G       G::::GU::::::U   U::::::U       Y:::::Y       ";
-echo "SSSSSSS     S:::::K:::::::K   K::::::K      Y:::::Y       G:::::GGGGGGGG::::GU:::::::UUU:::::::U       Y:::::Y       ";
-echo "S::::::SSSSSS:::::K:::::::K    K:::::K   YYYY:::::YYYY     GG:::::::::::::::G UU:::::::::::::UU     YYYY:::::YYYY    ";
-echo "S:::::::::::::::SSK:::::::K    K:::::K   Y:::::::::::Y       GGG::::::GGG:::G   UU:::::::::UU       Y:::::::::::Y    ";
-echo " SSSSSSSSSSSSSSS  KKKKKKKKK    KKKKKKK   YYYYYYYYYYYYY          GGGGGG   GGGG     UUUUUUUUU         YYYYYYYYYYYYY    ";
+echo " ######\  ##\   ##\ ##\     ##\  ######\  ##\   ##\ ##\     ##\ ";
+echo "##  __##\ ## | ##  |\##\   ##  |##  __##\ ## |  ## |\##\   ##  |";
+echo "## /  \__|## |##  /  \##\ ##  / ## /  \__|## |  ## | \##\ ##  / ";
+echo "\######\  #####  /    \####  /  ## |####\ ## |  ## |  \####  /  ";
+echo " \____##\ ##  ##<      \##  /   ## |\_## |## |  ## |   \##  /   ";
+echo "##\   ## |## |\##\      ## |    ## |  ## |## |  ## |    ## |    ";
+echo "\######  |## | \##\     ## |    \######  |\######  |    ## |    ";
+echo " \______/ \__|  \__|    \__|     \______/  \______/     \__|    ";
 
