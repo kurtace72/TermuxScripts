c2a0f2637900cd7afb0cb2f64815fdee0eb8fb65
RodrigoRosmaninho <quico.rosmaninho@gmail.com>
2016-11-30 18:19:07 +0000

    Changed structure of init.sh, main functionality complete

--- init/init.sh
+++ init/init.sh
@@ -2,6 +2,11 @@
 # Init script for my termux environment
 # Termux doesn't support logins so I'm writing this more for show than for anything else
 
+function exitTermux {
+	kill -9 $PPID
+	}
+
+echo ''
 echo "Quico's Android Terminal"
 echo ''
 
@@ -9,15 +14,15 @@ echo ''
 value=`cat $HOME/init/.lv426.txt | tr [:lower:] [:upper:] | tr [A-Za-z] [N-ZA-Mn-za-m] | tr '123456789' '987654321' | rev`
 IFS='_' read -r -a valueArr <<< "$value"
 
-while true; do
-    read -p "LOGIN: " u
+read -p "LOGIN: " u
     case ${u^^} in
         ${valueArr[0]}* ) printf 'PASSWORD: '; read -s pass;
             if [ ${pass^^} = ${valueArr[1]} ]; then
-                echo ''; printf '\nWelcome, Quico.\n\n\n'; break; else printf "\n** Code 403 **\n"; exit; fi;;
-        * ) echo "** Code 403 **"; exit;;
+                echo ''; printf '\nWelcome, Quico.\n\n\n'; 
+break; else printf "\n** Code 403 **\n"; exitTermux; fi;;
+        * ) echo "** Code 403 **"; exitTermux;;
     esac
-done
+
 
 clear
 
@@ -29,4 +34,6 @@ echo " \____##\ ##  ##<      \##  /   ## |\_## |## |  ## |   \##  /   ";
 echo "##\   ## |## |\##\      ## |    ## |  ## |## |  ## |    ## |    ";
 echo "\######  |## | \##\     ## |    \######  |\######  |    ## |    ";
 echo " \______/ \__|  \__|    \__|     \______/  \______/     \__|    ";
+echo ''
+echo 'Logged In!'
 echo ''
